422afd19527f530c109bf0f32dd19516adfe5d7e
sahithvibudhi <v.sahithkumar@gmail.com>
2021-06-26 19:08:43 -0500

    multi stage docker for go server

--- boiler-plates/go-pgsql-microservice/handlers/setup.go
+++ boiler-plates/go-pgsql-microservice/handlers/setup.go
@@ -10,18 +10,19 @@ import (
 
 func Setup() error {
 	api := rest.NewApi()
+
 	router, err := rest.MakeRouter(
 		rest.Get("/users", getUsers),
 		rest.Post("/users", postUser),
 		rest.Delete("/users/:user-id", deleteUser),
 	)
-
 	if err != nil {
 		helpers.Log.Error("Could not setup rest router. Err: %v", err)
 		return err
 	}
 
 	api.SetApp(router)
+
 	helpers.Log.WithFields(logrus.Fields{"port": 8080}).Info("Server is up!")
 
 	err = http.ListenAndServe(":8088", api.MakeHandler())
